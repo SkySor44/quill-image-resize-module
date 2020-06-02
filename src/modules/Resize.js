03bf0a78c6a890a6bd8b93c1d71137ac1f180e15
Skyler Sorensen <skyler.sorensen@gargle.com>
2020-06-02 10:14:50 -0600

    change style of figure

--- src/modules/Resize.js
+++ src/modules/Resize.js
@@ -90,6 +90,7 @@ export class Resize extends BaseModule {
 		if (this.dragBox === this.boxes[0] || this.dragBox === this.boxes[3]) {
 			// left-side resize handler; dragging right shrinks image
 			this.img.width = Math.round(this.preDragWidth - deltaX);
+			this.img.style.width = Math.round(this.preDragWidth - deltaX);
 			if (
 				this.img.firstChild &&
 				this.img.firstChild.tagName.toUpperCase() === "IMG"
@@ -114,6 +115,7 @@ export class Resize extends BaseModule {
 		} else {
 			// right-side resize handler; dragging right enlarges image
 			this.img.width = Math.round(this.preDragWidth + deltaX);
+			this.img.style.width = Math.round(this.preDragWidth + deltaX);
 			if (
 				this.img.firstChild &&
 				this.img.firstChild.tagName.toUpperCase() === "IMG"
